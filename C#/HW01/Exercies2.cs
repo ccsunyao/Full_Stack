6947fa1145b76424298977f5a09535978519a025
Lloyd <32090105+ccsunyao@users.noreply.github.com>
2021-05-19 23:04:11 -0500

    Add files via upload

---
+++ C#/HW01/Exercies2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Exercise2
+{
+    public class Arithmetic
+    {
+        int a, b;
+        public void GetData()
+        {
+            Console.WriteLine("Enter Integer a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Integer b");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void MathsChoice()
+        {
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Subtraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Divsion");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic a1 = new Arithmetic();
+
+            int a, b;
+
+            Console.WriteLine("Enter Integer a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Integer b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            a1.MathsChoice();
+            int c = Convert.ToInt16(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    Console.WriteLine("Addition Of Two Numbers : " + (a + b));
+                    break;
+                case 2:
+                    Console.WriteLine("Subtraction Of Two Numbers : " + (a - b));
+                    break;
+                case 3:
+                    Console.WriteLine("Multiplicaion Of Two Numbers : " + (a * b));
+                    break;
+                case 4:
+                    Console.WriteLine("Division Of Two Numbers : " + (a / b));
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/HW01/Exercies5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class BianryTriangle
+    {
+        static void Main(string[] args)
+        {
+            int p, lastInt = 0, input;
+            Console.WriteLine("Enter the Number of Rows : ");
+            input = Convert.ToInt16(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                for (p = 1; p <= i; p++)
+                {
+                    if (lastInt == 1)
+                    {
+                        Console.Write("0");
+                        lastInt = 0;
+                    }
+                    else if (lastInt == 0)
+                    {
+                        Console.Write("1");
+                        lastInt = 1;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
