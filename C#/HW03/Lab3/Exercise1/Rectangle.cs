2c74139a5a181beb2a2411298b8fb7016f653597
ccsunyao <ccsunyao@gmail.com>
2021-05-23 20:19:16 -0500

    C#HW03
    C# homework03

---
+++ C#/HW03/Lab3/Exercise1/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Rectangle : Shape1
+    {
+        float length = 1;
+        float width = 1;
+        public void GetData()
+        {
+        again:
+            Console.Write("Enter Length : ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Width : ");
+            width = Convert.ToInt32(Console.ReadLine());
+            if (length < 0 || length > 20 || width < 0 || width > 20)
+            {
+                Console.WriteLine("Please enter length and width between 0 and 20");
+                goto again;
+            }
+        }
+
+        public override float Area()
+        {
+            return length * width;
+        }
+
+        public override float Circumference()
+        {
+            return 2 * (length + width);
+        }
+    }
+}
